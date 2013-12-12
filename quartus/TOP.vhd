ea34a32cc7ef7e94eb3b89e80e05254a506d9936
sankerspace <e0325230@student.tuwien.ac.at>
2013-12-13 00:52:28 +0100

    Signalgenerator implemented but not succesfully compiled

--- quartus/TOP.vhd
+++ quartus/TOP.vhd
@@ -7,9 +7,9 @@ use ieee.std_logic_1164.all;
 entity top is
 
 port (
-   --CLK_50 :IN	std_logic;
+   CLOCK_50         :in	std_logic;
 	reset_pin		:in 	std_logic;
-	GPIO   			:out std_logic_vector(7 downto 0));
+	GPIO   			:out std_logic_vector(7 downto 0) );
 
 end entity;
 
@@ -18,19 +18,21 @@ end entity;
 
 
 architecture rtl of top is
-component signalgenerator is 
+component signalgenerator_2 is 
 	port(
+		clk		:in  std_logic							:= 'X';
 		reset 	:in 	std_logic                    := 'X'; 	-- clk
 		output 	:out	std_logic 				-- export
 	);
 
-end component signalgenerator;
+end component signalgenerator_2;
 --signal output_s  : std_logic;
 begin
-	signalgenerator_top : component signalgenerator
+	signalgenerator_top : component signalgenerator_2
 	port map (
 		output => GPIO(0),
-		reset => reset_pin
+		reset => reset_pin,
+		clk => CLOCK_50 
 	);
 	
 	--GPIO(0)<= output_s;
