0620a288cc27f7a41abdc47a39c9b7f8f17dc9f4
sankerspace <e0325230@student.tuwien.ac.at>
2013-12-14 23:40:44 +0100

    Signalgenerator seperated in concurrent processes

--- modelsim/testbenches/testbench_.vhd
+++ modelsim/testbenches/testbench_.vhd
@@ -5,10 +5,10 @@ USE ieee.std_logic_1164.all  ;
 USE ieee.std_logic_textio.all  ; 
 USE ieee.std_logic_unsigned.all  ; 
 USE std.textio.all  ; 
-ENTITY \testbench_\  IS 
+ENTITY \testbench_.vhd\  IS 
 END ; 
  
-ARCHITECTURE \testbench__arch\   OF \testbench_\   IS
+ARCHITECTURE \testbench_.vhd_arch\   OF \testbench_.vhd\   IS
   SIGNAL output   :  STD_LOGIC  ; 
   SIGNAL clk   :  STD_LOGIC  ; 
   SIGNAL reset   :  STD_LOGIC  ; 
@@ -28,35 +28,37 @@ BEGIN
 
 
 -- "Clock Pattern" : dutyCycle = 50
--- Start Time = 0 ns, End Time = 1 us, Period = 10 ns
+-- Start Time = 0 ns, End Time = 1 us, Period = 40 ns
   Process
 	Begin
 	 clk  <= '0'  ;
-	wait for 5 ns ;
--- 5 ns, single loop till start period.
-	for Z in 1 to 99
+	wait for 20 ns ;
+-- 20 ns, single loop till start period.
+	for Z in 1 to 24
 	loop
 	    clk  <= '1'  ;
-	   wait for 5 ns ;
+	   wait for 20 ns ;
 	    clk  <= '0'  ;
-	   wait for 5 ns ;
--- 995 ns, repeat pattern in loop.
+	   wait for 20 ns ;
+-- 980 ns, repeat pattern in loop.
 	end  loop;
 	 clk  <= '1'  ;
-	wait for 5 ns ;
+	wait for 20 ns ;
 -- dumped values till 1 us
 	wait;
  End Process;
 
 
 -- "Constant Pattern"
--- Start Time = 100 ns, End Time = 1 us, Period = 0 ns
+-- Start Time = 300 ns, End Time = 1 us, Period = 0 ns
   Process
 	Begin
 	 reset  <= '0'  ;
-	wait for 100 ns ;
+	wait for 200 ns ;
 	 reset  <= '1'  ;
-	wait for 900 ns ;
+	wait for 100 ns ;
+	 reset  <= '0'  ;
+	wait for 700 ns ;
 -- dumped values till 1 us
 	wait;
  End Process;
--- quartus/Signalgenerator_2.vhd
+++ quartus/Signalgenerator_2.vhd
@@ -11,76 +11,107 @@ port (
    clk 		:in	std_logic;
 	reset		:in 	std_logic;
 	--divider	:in 	std_logic_vector(3 downto 0);
-	output	:out  std_logic);
+	output	:out  std_logic :='0');
 
 end entity;
 
 
 architecture Behavioural of signalgenerator_2 is
+--Declaration in the whole architecture
 constant counter_max : integer := 10;
-begin
+shared variable counter : integer := 0;
+shared variable j : integer := 0;
+shared variable n : integer := 1;
+shared variable is_output_cycle : boolean := false;
+shared variable periodic_cylce : integer :=0;
 
-generator : process (clk,reset) is
-
-variable counter : integer := 0;
-variable j : integer := 0;
-variable n : integer := 1;
-variable periodic_cylce : integer :=0;
-variable is_output_cycle : integer := 0;
+signal drive_output : std_logic := '0';
+begin --BEGIN ARCHITECTURE
+  
+-- works parallel to the other processes
+drive_out: process (clk) is
 begin
-	--wait until rising_edge(clk);
-			--exit when reset='1';
-	if (reset = '1') then
-		n:=1;
-		j:=0;
-		output <= '0';
-		is_output_cycle := 0;
-	
-	elsif(clk'event and clk='1') then	
-	
-    	if( is_output_cycle = 0) then
+  if(rising_edge(clk))then
+    --output <= '1' ;
+  end if;
+end process drive_out;
+
+
+-- COMBINATORICAL DATAFLOW
+generator_controll : process (clk) is --  BEGIN PROCESS GENERATORE
+begin
+    if(clk'event) then
+     -- controlling flow 
+    	if( is_output_cycle = false and reset = '0') then
     		-- controll n
     		if n = counter_max then  
     			n := 1;
-    			j:=0;--will be incremented in th next if statement
+    			j := 0;--will be incremented in th next if statement
     		end if;
     		--controll j
     		if(j < n) then --if j reaches maximum n , than n will be incremented
     			j := j + 1;
     		else
-    			j:=1;
+    			j := 1;
     			n := n + 1;
     		end if;
     		-- intialize anything else
-    		is_output_cycle := 1;
     		periodic_cylce := 0;
     		counter := j;
+    		is_output_cycle :=  true;    		
     	end if;
-    	
-    	
-    	if(is_output_cycle = 1 and clk'event)  then -- this is processing part which controls output
-    		if(periodic_cylce = 0) then -- first half of a period starts with output = 0
-    			if (counter > 0) then
-    				output <= '0';
-    				counter := counter - 1;
-    			else
-    				counter := j; --prepare for next periodic cycle
-    				periodic_cylce := 1;
-    			end if;
-    		end if;
-    
-    		if(periodic_cylce = 1) then-- second half of a period starts with output = 1
-    			if(counter > 0) then
-    				output <= '1';
-    				counter := counter - 1;
-    			else
-    				is_output_cycle := 0;
-    			end if;
-    		end if;
-    	end if;
-    	
- 	end if;
-				
+  	end if;
+end process generator_controll; --  END PROCESS GENERATORE
 
-end process generator;
-end architecture ;
+-- COMBINATORICAL DATAFLOW
+generator_drive: process(clk) -- BEGIN PROCESS SYNC
+variable initialize : boolean := true ;
+begin
+  if(initialize = true) then
+      drive_output <= '0';
+      initialize := false;
+  end if;
+  if(rising_edge(clk)) then
+    	-- driven flow
+    	if(is_output_cycle = true and reset = '0')  then -- this is processing part which controls output
+      		if(periodic_cylce = 0) then -- first half of a period starts with output = 0
+      			if (counter > 0) then
+      				output <= '0';
+      				counter := counter - 1;
+      			else
+      				counter := j; --prepare for next periodic cycle
+      				periodic_cylce := 1;
+      			end if;
+      		end if;
+      
+      		if(periodic_cylce = 1) then-- second half of a period starts with output = 1
+      			if(counter > 0) then
+      				output <= '1';
+      				counter := counter - 1;
+      			else
+      				is_output_cycle := false;
+      			end if;
+      		end if;
+   	 end if;
+ 	end if;
+end process generator_drive; -- END PROCESS SYNC
+
+-- REGISTERED PROCESS
+reset_process : process(reset) is -- BEGIN PROCESS RESET_PROCESS
+begin
+  if(reset = '1') then 
+    n := 1;
+	  j := 0;
+	  --output <= '0';
+	  is_output_cycle := true;
+	end if;
+end process reset_process; -- END PROCESS RESET_PROCESS
+
+
+
+
+
+
+
+
+end architecture ; --END ARCHITECTURE
