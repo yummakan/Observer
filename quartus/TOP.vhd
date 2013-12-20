fd46c9b387b50b5172b59d0511cd7df4d8829ba2
Marko Stanisic <marko@ecslab5.ecs.tuwien.ac.at>
2013-12-20 16:32:21 +0100

    Status of project before Reorganisation: -) copied project file from MyFirstProject and substitute current project file -) used sdc file from MyFirstProject
    next step , create new Project

--- quartus/TOP.vhd
+++ quartus/TOP.vhd
@@ -8,6 +8,8 @@ entity top is
 
 port (
    CLOCK_50         :in	std_logic;
+	CLOCK2_50         :in	std_logic;
+	CLOCK3_50         :in	std_logic;
 	reset_pin			:in 	std_logic;
 	GPIO   			:out std_logic_vector(7 downto 0) );
 
@@ -18,25 +20,27 @@ end entity;
 
 
 architecture rtl of top is
-component signalgenerator is 
-	port(
-		clk		:in  std_logic							:= 'X';
-		reset 	:in 	std_logic                    := 'X'; 	-- clk
-		output 	:out	std_logic 				-- export
-);
+--component signalgenerator is 
+--	port(
+--		clk		:in  std_logic							:= 'X';
+--		reset 	:in 	std_logic                    := 'X'; 	-- clk
+--		output 	:out	std_logic 				-- export
+--);
 
-end component signalgenerator;
+--end component signalgenerator;
 signal clk_s  : std_logic;
 begin
-	signalgenerator_top : component signalgenerator
-	port map (
-		output => GPIO(1),
-		reset => reset_pin,
-		clk => clk_s  
-	);
-	clk_s <= CLOCK_50;
+--	signalgenerator_top : component signalgenerator
+--	port map (
+--		output => GPIO(1),
+--		reset => reset_pin,
+--		clk => clk_s  
+--	);
+	GPIO(0) <= CLOCK_50;
+	GPIO(1) <= CLOCK2_50;
+	GPIO(2) <= CLOCK3_50;
 	--GPIO(0) <= clk_s ;
-	--GPIO(1) <= clk_s;
+	--GPIO(0) <= clk_s;
 	--GPIO(2) <= reset_pin;
 	
 	--GPIO(0)<= output_s;
