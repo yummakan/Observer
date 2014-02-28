4d69f5c89421cb63a051edc7a87c71cc045b5b87
sankerspace <e0325230@student.tuwien.ac.at>
2014-02-28 04:18:27 +0100

    introduced documentation document

---
+++ Documentation/document/template-1/saeubern.bat
@@ -0,0 +1,24 @@
+del *.aux
+del *.log
+del *.toc
+del *.lof
+del *.bak
+del *.loa
+del *.log
+del *.lot
+del *.bbl
+del *.blg
+del *.idx
+del *.ilg
+del *.ind
+del *.dvi
+del *.out
+del *.brf
+del *.thm
+del *.toc
+del thumbs.db
+del  *.synctex.gz
+
+del literatur-template.tex
+
+
